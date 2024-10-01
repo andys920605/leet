ee77c6cf02cc8b36256807a5fe44cbc697a64c90
AndyChang <andy.chang@framy.co>
2024-10-01 10:42:36 +0800

    27

--- 27/main.go
+++ 27/main.go
@@ -1,7 +1,20 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	nums := []int{3, 2, 2, 3}
+	val := 3
+	fmt.Println(removeElement(nums, val))
+}
 
 func removeElement(nums []int, val int) int {
-	return 0
+	count := 0
+	for _, num := range nums {
+		if num != val {
+			nums[count] = num
+			count++
+		}
+	}
+	return count
 }
